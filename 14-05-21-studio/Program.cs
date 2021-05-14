e9485cc98b1c313142c7e282f0e750e284507beb
Jakeclearwater <jakeclearwater7@gmail.com>
2021-05-14 13:58:37 +1200

    change

--- 14-05-21-studio/Program.cs
+++ 14-05-21-studio/Program.cs
@@ -11,6 +11,13 @@ namespace _14_05_21_studio
             int age;
 
 
+
+
+
+
+            Console.WriteLine($"Your name is {name} and you are {age} years old");
+            Console.ReadLine();
+
         }
     }
 }

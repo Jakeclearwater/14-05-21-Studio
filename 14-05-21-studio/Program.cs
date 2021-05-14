6dc0c368cc37dbf06b4a7c815cfcfd8e8b9ec6b7
lukepickard18 <lukepc445@gmail.com>
2021-05-14 13:51:32 +1200

    crep check

--- 14-05-21-studio/Program.cs
+++ 14-05-21-studio/Program.cs
@@ -9,8 +9,9 @@ namespace _14_05_21_studio
 
             string temp, name;
             int age;
-
-
+            Console.WriteLine(" Please enter your age!");
+            temp = Console.ReadLine();
+            age = Convert.ToInt32(temp);
         }
     }
 }
